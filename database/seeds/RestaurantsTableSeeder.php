cd5bf921687d254687d6dcced8ce68846ef42c1f
Antoine Marchais <antoinemarchais@MacBook-Pro-de-Antoine.local>
2018-06-21 13:20:35 +0200

    add seeds

---
+++ database/seeds/RestaurantsTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class RestaurantsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+
+        // ==============================================================================
+        // CREATE DUMMY OFFERS FOR TESTING
+        // ==============================================================================
+        foreach (range(1, 50) as $index) {
+            DB::table('restaurant')->insert([
+                'name'          => $faker->sentence(3),
+                'address'       => $faker->address,
+                'phone_number'  => $faker->e164phoneNumber,
+                'rating'        => $faker->randomDigit,
+                'image'         => $faker->imageUrl(400,200),
+                'created_at'    => $faker->dateTime($max = 'now')
+            ]);
+        }
+    }
+}
